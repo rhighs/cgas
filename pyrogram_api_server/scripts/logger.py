6decaf1bc2341b2e0bfdedeee4b9ac4b0c38b54b
Roberto Montalti <roberto.montalti00@gmail.com>
2021-05-27 15:07:42 +0200

    added simple printing

--- pyrogram_api_server/scripts/logger.py
+++ pyrogram_api_server/scripts/logger.py
@@ -9,18 +9,5 @@ class Loggeer:
     def log(self, message: str):
         print(self.success_string(message))
 
-    def error():
-
-        pass
-
-def print_format_table():
-    for style in range(8):
-        for fg in range(30,38):
-            s1 = ''
-            for bg in range(40,48):
-                format = ';'.join([str(style), str(fg), str(bg)])
-                s1 += '\x1b[%sm %s \x1b[0m' % (format, format)
-            print(s1)
-        print('\n')
-         
-print_format_table()
+    def error(self, message: str):
+        print(message)
