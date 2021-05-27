6448432be71d218ae97cd8725f0ecc4f78a4918a
Roberto Montalti <roberto.montalti00@gmail.com>
2021-05-27 12:01:08 +0200

    add user prompt for api keys

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from pyrogram_api_server import ApiServer
 
-app = ApiServer(api_id="", api_hash="", port=5000, host_ip="0.0.0.0")
+api_id = input("Your api_id: ")
+api_hash= input("Your api_hash: ")
+
+app = ApiServer(api_id, api_hash, port=5000, host_ip="0.0.0.0")
 app.run()
