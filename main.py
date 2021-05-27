969a84f0729f9a941fd9f128bc08b10aa984abbd
Roberto Montalti <roberto.montalti00@gmail.com>
2021-05-27 16:10:04 +0200

    added simple commands on --help

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import sys
+from pyrogram_api_server import composeHelp
 from pyrogram_api_server import ApiServer
+
+if(len(sys.argv) > 1):
+    command = sys.argv[1]
+    if(command == "--help"):
+        print(composeHelp())
+        exit(0)
+    elif(command == "--no-keys"):
+        app = ApiServer(port=5000, host_ip="0.0.0.0", mode="no-keys")
+        app.run()
+        exit(0)
 
 api_id = input("Your api_id: ")
 api_hash = input("Your api_hash: ")
-
 app = ApiServer(api_id, api_hash, port=5000, host_ip="0.0.0.0")
 app.run()
--- pyrogram_api_server/__init__.py
+++ pyrogram_api_server/__init__.py
@@ -3,3 +3,4 @@ from pyrogram_api_server.scripts        import PyroWrap
 from pyrogram_api_server.api_server     import ApiServer
 from pyramid.config                     import Configurator
 from .api_server                        import getPyroWrapper
+from .cmd                               import composeHelp
