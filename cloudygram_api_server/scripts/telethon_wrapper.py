50c87f15d43fb2ecfc1ec78925f00b0dbc3d4b5e
robertomontalti <roberto.montalti00@gmail.com>
2021-05-30 23:34:13 +0200

    added download method, still a bit broken... totally broken

--- cloudygram_api_server/scripts/telethon_wrapper.py
+++ cloudygram_api_server/scripts/telethon_wrapper.py
@@ -4,6 +4,8 @@ from pyrogram                       import types
 from os                             import path
 from cloudygram_api_server.models   import TtModels
 from telethon.tl.types.auth         import SentCode
+from telethon.utils import get_input_location
+from telethon.tl.types import Document
 
 class TtWrap:
     def __init__(self, api_id, api_hash):
@@ -15,9 +17,9 @@ class TtWrap:
 
     def send_private_message(self, phone_number, message):
         client = self.create_client(phone_number) 
+        client.connect()
         if not client.is_user_authorized():
             return
-        client.connect()
         client.send_message("me", message)
         client.disconnect()
 
@@ -28,9 +30,9 @@ class TtWrap:
 
     async def send_code(self, phone_number):
         client = self.create_client(phone_number)
+        await client.connect()
         if not client.is_user_authorized():
             raise Exception("Invalid phone number, not authenticated")
-        await client.connect()
         try:
             code: SentCode = await client.send_code_request(phone_number)
         except Exception as e :
@@ -41,9 +43,9 @@ class TtWrap:
 
     async def signin(self, phone_number, phone_code_hash, phone_code):
         client = self.create_client(phone_number)
+        await client.connect()
         if not client.is_user_authorized():
             raise Exception("Invalid phone number, not authenticated")
-        await client.connect()
         try:
             result: types.SentCode = await client.sign_in(phone=phone_number, phone_code_hash=phone_code_hash, code=phone_code)
         except Exception as e:
@@ -57,18 +59,30 @@ class TtWrap:
 
     async def get_me(self, phone_number):
         client = self.create_client(phone_number)
+        await client.connect()
         if not client.is_user_authorized():
             raise Exception("Invalid phone number, not authenticated")
-        await client.connect()
         result = await client.get_me()
         await client.disconnect()
         return result
 
-    def upload_file(self, phone_number, file_path):
+    async def upload_file(self, phone_number, file_stream):
         client = self.create_client(phone_number)
-        if not client.is_user_authorized():
+        await client.connect()
+        if not await client.is_user_authorized():
+            await client.disconnect()
             raise Exception("Invalid phone number, not authenticated")
-        client.connect()
-        result = client.upload_file(file=None)
-        client.disconnect()
-        return result
+        result = await client.upload_file(file=file_stream)
+        print(f"uploaded file for {phone_number} with id: {result.id} and md5 {result.md5_checksum} and parts {result.parts}")
+        await client.disconnect()
+
+    async def download_file(self, phone_number, file_id):
+        client = self.create_client(phone_number)
+        await client.connect()
+        doc = Document(id=-8114692116561779292, access_hash=0, date=None, file_reference="", mime_type="text/plain", size=20, dc_id=1, attributes=[])
+        dc_id, loc  = get_input_location(doc)
+        if not await client.is_user_authorized():
+            await client.disconnect()
+            raise Exception("Invalid phone number, not authenticated")
+        await client.download_file(loc)
+        await client.disconnect() 
