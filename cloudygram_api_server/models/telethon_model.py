5003ac271ff9f1bac6ba1b5ce1d79838dfb5b2c6
roberto montalti <roberto.montalti00@gmail.com>
2021-06-29 23:23:11 +0200

    updated telethon models

--- cloudygram_api_server/models/telethon_model.py
+++ cloudygram_api_server/models/telethon_model.py
@@ -1,4 +1,5 @@
-from .constants import SUCCESS_KEY, MESSAGE_KEY
+from .constants                     import SUCCESS_KEY, MESSAGE_KEY, DATA_KEY
+from cloudygram_api_server.scripts  import CGMessage
 
 class TtModels:
     @staticmethod
@@ -13,4 +14,16 @@ class TtModels:
         return {
             SUCCESS_KEY : False,
             MESSAGE_KEY : message
+        }
+
+    @staticmethod
+    def message_list(messages):
+        mapped_list = map(
+                CGMessage.map_from_tt,
+                messages
+                )
+
+        return {
+            SUCCESS_KEY: True,
+            DATA_KEY: mapped_list
         }
