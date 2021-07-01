995f7c7f3e73f40b0b11ebdb829f6854b1471051
roberto montalti <roberto.montalti00@gmail.com>
2021-07-02 01:25:54 +0200

    added signup call

--- cloudygram_api_server/controllers/home_controller.py
+++ cloudygram_api_server/controllers/home_controller.py
@@ -14,13 +14,13 @@ class HomeController(object):
 
     def __init__(self, request: Request):
         self.pool = concurrent.futures.ThreadPoolExecutor()
+        self.wrap = cloudygram_api_server.get_tt()
         self.request = request
-        
+
     @action(name="addSession", renderer="json", request_method="GET")
     def add_account(self):
         phoneNumber = self.request.GET["phoneNumber"][1:]
-        wrap = cloudygram_api_server.get_tt()
-        wrap.create_session(phoneNumber)
+        self.wrap.create_session(phoneNumber)
         return HomeModels.success(message=f"Session with: {phoneNumber} created.")
 
     @action(name="sendCode", renderer="json", request_method="GET")
@@ -28,9 +28,9 @@ class HomeController(object):
         phone_number = self.request.GET["phoneNumber"][1:]
         wrap = cloudygram_api_server.get_tt()
         result = self.pool.submit(
-            asyncio.run,
-            wrap.send_code(phone_number)
-        ).result()
+                asyncio.run,
+                self.wrap.send_code(phone_number)
+                ).result()
         if type(result) is dict and SUCCESS_KEY in result:
             return result
         return HomeModels.sent_code(result)
@@ -41,13 +41,13 @@ class HomeController(object):
         phone_number = body["phoneNumber"][1:]
         phone_code_hash = body["phoneCodeHash"]
         phone_code = body["phoneCode"]
-        wrap = cloudygram_api_server.get_tt()
+        self.wrap = cloudygram_api_server.get_tt()
         result = self.pool.submit(
-            asyncio.run,
-            wrap.signin(phone_number=phone_number,
-                        phone_code_hash=phone_code_hash,
-                        phone_code=phone_code)
-        ).result()
+                asyncio.run,
+                self.wrap.signin(phone_number=phone_number,
+                    phone_code_hash=phone_code_hash,
+                    phone_code=phone_code)
+                ).result()
         if type(result) is dict and SUCCESS_KEY in result:
             return result
         return UserModels.userDetails(result)
@@ -55,10 +55,30 @@ class HomeController(object):
     @action(name="qrLogin", renderer="json", request_method="GET")
     def qr_login(self):
         phone_number=self.request.GET["phoneNumber"]
-        wrap = cloudygram_api_server.get_tt()
         result = self.pool.submit(
-            asyncio.run,
-            wrap.qr_login(phone_number)
-        ).result()
+                asyncio.run,
+                self.wrap.qr_login(phone_number)
+                ).result()
         self.pool.submit(asyncio.run, result.wait())
         return UserModels.success(data=result.url)
+
+    @action(name="singup", renderer="json", request_method="POST")
+    def sign_up(self):
+        body = self.request.json_body
+        code = body["phoneCode"]
+        first_name = body["firstName"]
+        last_name = body["lastName"]
+        phone_code_hash = body["phoneCodeHash"]
+        result = self.pool.submit(
+                asyncio.run,
+                self.wrap.signup(
+                    code,
+                    first_name,
+                    last_name,
+                    phone_code_hash=phone_code_hash
+                    )
+                ).result()
+        return UserModels.userDetails(result)
+
+
+
