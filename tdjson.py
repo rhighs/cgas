4219110ae940716fbaef7db571423c2d503457a2
roberto montalti <roberto.montalti00@gmail.com>
2021-07-17 09:44:33 +0200

    ooppsieeee

--- tdjson.py
+++ tdjson.py
@@ -2,10 +2,10 @@ from telegram.client import Telegram
 from telegram.tdjson import TDJson
 tdj = TDJson() #to use later
 tg = Telegram(
-        api_id=5558646,
-        api_hash="30d3b4c9958fd165911d09254124d3bc",
-        phone='+393421323295',
-        database_encryption_key='changekey123',
+        api_id=0,
+        api_hash="",
+        phone="",
+        database_encryption_key="",
         )
 tg.login()
 result = tg.get_chats()
