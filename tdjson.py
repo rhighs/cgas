5306772101b19990107f13a6f54c48a778afe4df
roberto montalti <roberto.montalti00@gmail.com>
2021-07-17 10:03:31 +0200

    added lib location

--- tdjson.py
+++ tdjson.py
@@ -1,5 +1,18 @@
 from telegram.client import Telegram
 from telegram.tdjson import TDJson
+
+from ctypes import c_int, c_char_p, c_double, c_voidp, c_longlong
+from typing import Any, Dict, Optional, Union
+import pkg_resources
+
+def locate_clib() -> str:
+    import platform
+    if platform.system().lower() != "darwin":
+        lib_path = "/usr/lib/libtdjson.so"
+    else:
+        return None
+    return pkg_resources.resource_filename("telegram", lib_path)
+
 tdj = TDJson() #to use later
 tg = Telegram(
         api_id=0,
