8a96899078f2859f74052b71cc7b0ccf530bc835
roberto montalti <roberto.montalti00@gmail.com>
2021-09-16 01:04:28 +0200

    created a file for payload keys in controller messages, both requests and responses

---
+++ cloudygram_api_server/payload_keys.py
@@ -0,0 +1,17 @@
+from types import SimpleNamespace
+
+tg_data     = SimpleNamespace()
+file        = SimpleNamespace()
+dl          = SimpleNamespace()
+
+tg_data.phone       =   "phoneNumber"
+tg_data.code        =     "phoneCode"
+tg_data.code_hash   = "phoneCodeHash"
+tg_data.fname       =     "firstName"
+tg_data.lname       =      "lastName"
+
+file.name =     "file"
+file.mime = "mimeType"
+file.path =     "path"
+
+dl.message = "message"
