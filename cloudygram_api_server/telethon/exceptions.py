62505fbdd875c4271a17890ef7b7fb9dc1ddfcb5
roberto montalti <roberto.montalti00@gmail.com>
2021-11-08 17:12:34 +0100

    added minimal exceptions for some behaviours of the wrapper

---
+++ cloudygram_api_server/telethon/exceptions.py
@@ -0,0 +1,23 @@
+
+class TTUnathorizedException(Exception):
+    def __init__(self, message: str = None):
+        if message == None:
+            message = "The user trying to access telegram features is not authorized."
+        super().__init__(message)
+
+class TTGenericException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class TTSignInException(Exception):
+    def __init__(self, message: str = None):
+        if message == None:
+            message = "Some of the provided codes isn't valid for authentication."
+        super().__init__(message)
+
+class TTFileTransferException(Exception):
+    def __init__(self, message: str = None):
+        if message == None:
+            message = "Some network error occured while transferring the file."
+        super().__init__(message)
+
