918ee349f8fb5a8e70cb7f2bf333ec53d75800ce
Roberto Montalti <roberto.montalti00@gmail.com>
2022-05-15 17:43:18 +0200

    Accept command args to switch between ASGI and WSGI

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
 import json
+import click
+import uvicorn
 from cloudygram_api_server import ApiServer
 from os import path, makedirs
 
 PATH = "keys.json"
 
-def startup():
+@click.command()
+@click.option("--asgi", is_flag=True)
+def startup(asgi: bool):
     if path.exists(PATH):
         with open(PATH, "r") as f:
             data = json.load(f)
@@ -17,8 +21,14 @@ def startup():
     if not path.exists("./sessions"):
         makedirs("./sessions")
 
-    app = ApiServer(data["api_id"], data["api_hash"], port=5000, host_ip="0.0.0.0")
-    app.run()
+    app = ApiServer(data["api_id"], data["api_hash"], port=5000, host_ip="127.0.0.1")
+
+    if asgi:
+        print("Starting ASGI server")
+        uvicorn.run(app.create_asgi_app(), host="0.0.0.0", port=5000)
+    else:
+        print("Starting WSGI server")
+        app.run()
 
 if __name__ == "__main__":
     startup()
